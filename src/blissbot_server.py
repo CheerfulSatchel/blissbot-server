1d0c862b4eb1d68dc893c1625e92523183b9ea74
James Wu <therealjameswu@gmail.com>
2019-04-08 17:32:21 -0400

    Serve random stories and provide basic json responses

---
+++ src/blissbot_server.py
@@ -0,0 +1,38 @@
+import random
+import json
+from utilities.sitescraper import retrieve_article_urls
+from flask_api import FlaskAPI
+
+APP = FlaskAPI(__name__)
+
+NEWS_URLS = retrieve_article_urls()
+
+@APP.route('/example/')
+def example():
+    return {'hello': 'world'}
+
+@APP.route('/random-story/')
+def random_story():
+    print(NEWS_URLS)
+    if NEWS_URLS:
+        random_idx = random.randint(0, len(NEWS_URLS) - 1)
+        return success_message(NEWS_URLS[random_idx])
+    else:
+        return failure_message()
+
+def success_message(msg_contents):
+    return json.dumps({
+        'ok': True,
+        'body': msg_contents
+    })
+
+def failure_message():
+    # TODO: Better body error messages LOL
+    return json.dumps({
+        'ok': False,
+        'body': 'Something went wrong!'
+    })
+
+if __name__ == '__main__':
+    APP.run(debug=True, port=4390)
+    
