be37333f71ecd9b27d796e6f539d839f6d474521
James Wu <therealjameswu@gmail.com>
2019-04-21 21:00:25 -0400

    Add initial database setup using SQLAlchemy

---
+++ src/database/database.py
@@ -0,0 +1,17 @@
+import os
+import pymysql
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+pymysql.install_as_MySQLdb()
+
+Base = declarative_base()
+
+db_username = os.environ.get('BLISSBOT_DB_USERNAME')
+db_password = os.environ.get('BLISSBOT_DB_PASSWORD')
+db_name = os.environ.get('BLISSBOT_DB_NAME')
+db_endpoint = os.environ.get('BLISSBOT_DB_ENDPOINT')
+db_port = os.environ.get('BLISSBOT_DB_PORT')
+
+sql_engine = create_engine(
+    'mysql://{}:{}@{}/{}'.format(db_username, db_password, db_endpoint, db_name), echo=True)
---
+++ src/database/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+
+class Article(Base):
+    __tablename__ = 'articles'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    image_url = Column(String)
+    title_link = Column(String)
+    category = Column(String)
+    meta_content = Column(String)
+
+    def __repr__(self):
+        return '<Article(id="%s", title="%s", image_url="%s", title_link="%s", category="%s", meta_content="%s"'
+        % (self.id,
+           self.title,
+           self.image_url,
+           self.title_link,
+           self.category,
+           self.meta_content)
